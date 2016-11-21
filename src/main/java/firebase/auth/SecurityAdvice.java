f585612f0ee7bacf83dbd013688f479bd2e82e53
Tri Bui <tri.bui@pycogroup.com>
2016-11-21 13:42:18 +0700

    Fix NullPointerException error for non-authenticated requests

--- src/main/java/firebase/auth/SecurityAdvice.java
+++ src/main/java/firebase/auth/SecurityAdvice.java
@@ -1,5 +1,6 @@
 package firebase.auth;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
@@ -13,8 +14,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class SecurityAdvice {
     @ModelAttribute
     public FirebaseUser currentUser() {
-        OAuth2Authentication authentication = (OAuth2Authentication)SecurityContextHolder.getContext().getAuthentication();
-        OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) authentication.getDetails();
-        return (FirebaseUser)details.getDecodedDetails();
+        Authentication originAuthentication = SecurityContextHolder.getContext().getAuthentication();
+        if (originAuthentication == null) return null;
+        if (!originAuthentication.isAuthenticated()) return null;
+        if (!(originAuthentication instanceof OAuth2Authentication)) return null;
+
+        OAuth2Authentication authentication = (OAuth2Authentication)originAuthentication;
+        OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails)authentication.getDetails();
+        if (!(details.getDecodedDetails() instanceof FirebaseUser)) return null;
+
+        return (FirebaseUser) details.getDecodedDetails();
     }
 }
